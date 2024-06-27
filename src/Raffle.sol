90177b3fe08bc5012e3b9084f0c2879b9173564e
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-06-27 13:45:03 +0530

    Add: Block TimeStamp, Events, Custom Errors, State Variables

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -27,13 +27,40 @@
 pragma solidity ^0.8.13;
 
 contract Raffle{
+
+    /**Custom Errors */
     error Raffle_NotEnoughtEthSent();
 
-    
+    /**State Variables */
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 public s_raffleStartingTime;
 
-    function enterRaffle() external payable {
-        if(msg.value < i_entranceFee){
-            revert Raffle_NotEnoughtEthSent;
+    /**Events */
+    event EnterRaffle(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval){
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_raffleStartingTime = block.timestamp;
+    }
+
+    function enterRaffle() external payable{
+        if(msg.value > i_entranceFee){
+            revert Raffle_NotEnoughtEthSent();
         }
+        emit EnterRaffle(msg.sender);
+    }
+
+    function pickWinner() external {
+        if(block.timestamp - s_raffleStartingTime < i_interval){
+            revert();
+        }
+    }
+
+    /**Getter Function */
+
+    function getEntranceFee() external view returns(uint256){
+        return i_entranceFee;
     }
 }
