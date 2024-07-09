46211601b250721bdf23f4927abdb3878774e079
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-07-09 17:39:24 +0530

    Create a DeployRaffle script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// Importing using root directory path
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 
 contract DeployRaffle is Script{
 
-    function run() external returns(Raffle, HelperConfig) {
-        return new Raffle();
-    }
+    function run() public {}
+
+    // function deployRaffle() external returns(Raffle, HelperConfig){}
 }
