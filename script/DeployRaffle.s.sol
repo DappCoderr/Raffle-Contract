7e91748081924cc6893b45fb912a88c6449530b0
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-07-10 14:56:50 +0530

    Update the deployRaffle script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,10 +4,29 @@ pragma solidity ^0.8.13;
 // Importing using root directory path
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
+import {HeHelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script{
 
     function run() public {}
 
-    // function deployRaffle() external returns(Raffle, HelperConfig){}
+    function deployRaffle() external returns(Raffle, HelperConfig){
+        HelperConfig helperConfig = new HeHelperConfig();
+        //local -> Deploy Mock, get local config
+        //Sepolia -> get sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrf_coordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return(raffle, helperConfig);
+    }
 }
